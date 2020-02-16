4c84a352236f39bef929cacb8ed359d5b07b8973
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-16 21:15:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  base_hash[:pieces] = "scerp"
+  base_hash[:railroads][:pieces] = "scerp"
 end
 
 def monopoly_with_third_tier
